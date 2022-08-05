5de28cb2f0abe7f1b34246e572b0d178d9ada049
Shen Z <shenAwesome@gmail.com>
2022-08-06 02:09:56 +1000

    gogo

--- test/vite.config.ts
+++ test/vite.config.ts
@@ -1,10 +1,11 @@
 
 //import { viteSingleFile } from "vite-plugin-singlefile"
 import { defineConfig } from 'vite'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 //import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
     base: '',
     //plugins: [vue()]
-    //plugins: [viteSingleFile()],
+    plugins: [viteSingleFile()],
 }) 
